e2f87a41a7099a9bc75daeeb4af477c3aa246e8e
JP <yns@gmail.Com>
2024-03-11 16:03:18 +0100

    scrap final

--- code_propre.py
+++ code_propre.py
@@ -5,35 +5,6 @@ import csv
 
 
 
-# # Remplacez 'url_de_la_page' par l'URL réelle de la page que vous souhaitez scraper.
-# url_de_la_page = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
-
-# response = requests.get(url_de_la_page)
-# html = response.content
-
-# soup = BeautifulSoup(html, 'html.parser')
-# table_rows = soup.find_all('tr')
-
-# data = []
-
-# for row in table_rows:
-#     cols = row.find_all('td')
-#     cols = [ele.text.strip() for ele in cols]
-#     data.append(cols)  # Get rid of empty values
-
-# for row in data: 
-#     print(row)
-
-
-# print(data)
-#table_musique=find_all("table",{"class"="wikitable sortable jquery-tablesorter"})
-#ligne_musique= offre.append(annonce.find("th", {"class": "headsort"}).find_next_sibling("tr")
-                            #numero_musique=find_all("tr").get_text())
-
-
-
-
-
 url_de_la_page = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
 
 response = requests.get(url_de_la_page)
@@ -60,42 +31,8 @@ for row in data:
 
 
 
-data_musique = pd.DataFrame(data, columns = ['N°', 'Date', 'Artiste', 'Titre', 'Nombre de semaines n°1', 'Commenraires'])
+data_musique = pd.DataFrame(data, columns = ['N°', 'Date', 'Artiste', 'Titre', 'Nombre de semaines n°1', 'Commentaires'])
 
 
 
 data_musique.to_csv("/home/ensai/Bureau/Conception de logiciel/projet/projet_conception_logiciel/Musique.csv",sep=";",index_label='id')
-
-
-# url_de_la_page = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
-
-# response = requests.get(url_de_la_page)
-# html = response.content
-
-# soup = BeautifulSoup(html, 'html.parser')
-# table_rows = soup.find_all('tr')
-
-# data = []
-# scraping_started = False
-
-# for row in table_rows:
-#     if "1" in row.text:
-#         scraping_started = True
-#     if scraping_started:
-#         cols = row.find_all('td')
-        
-#         # Vérifiez que la liste cols a suffisamment d'éléments avant d'accéder à un élément spécifique
-#         if len(cols) >= 2:
-#             cols = [ele.text.strip() for ele in cols]
-            
-#             # Group the dates in the same column
-#             date_range = cols[1].replace(',', ' ').split()
-#             cols[1] = '-'.join(date_range)
-            
-#             data.append(cols)
-
-# data_salaries = pd.DataFrame(data, columns=['ID', 'Dates', 'Artist', 'Title', 'Weeks', 'Notes'])
-
-# data_salaries.to_csv("/home/ensai/Bureau/Conception de logiciel/projet/projet_conception_logiciel/Emploi2.csv", sep=";", index_label='id')
-
-# print(data_salaries)
