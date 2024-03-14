9aa39c3adae74b5833ba2ec9613516a9605a6d87
JP <yns@gmail.Com>
2024-03-14 12:48:16 +0100

    fd

--- tests_unitaires.py
+++ tests_unitaires.py
@@ -99,3 +99,81 @@ class TestWikipediaScraper(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+# class SpotifyAPI:
+#     def __init__(self):
+#         self.sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+#     def recherche_piste(self, titre, artiste):
+#         query = f"{titre} {artiste}"
+#         results = self.sp.search(query, type='track', limit=1)
+#         if results['tracks']['items']:
+#             return results['tracks']['items'][0]['uri']
+#         else:
+#             return None
+
+#     def obtenir_audio(self, uri):
+#         track = self.sp.track(uri)
+#         return track['preview_url']
+
+#     def renvoyer_piste_audio(self, titre, artiste):
+#         uri = self.recherche_piste(titre, artiste)
+
+#         if uri:
+#             url = self.obtenir_audio(uri)
+#             print(url)
+#             return url
+#         else:
+#             print(f"Piste non trouvée pour {titre} de {artiste}")
+#             return None
+
+
+
+# # Exemple avec une recherche de piste
+# spotify_api = SpotifyAPI()
+# titre = "Shape of You"
+# artiste = "Ed Sheeran"
+# spotify_api.renvoyer_piste_audio(titre, artiste)
+    
+
+
+# class WikipediaScraper:
+#     def __init__(self, url):
+#         self.url = url
+#         self.response = requests.get(self.url)
+#         self.html = self.response.content
+#         self.soup = BeautifulSoup(self.html, 'html.parser')
+#         self.table_rows = self.soup.find_all('tr')
+#         self.data = []
+#         self.scraping_started = False
+
+#     def scrape_data(self, target_date):
+#         for row in self.table_rows:
+#             if target_date in row.text:
+#                 self.scraping_started = True
+#             if self.scraping_started:
+#                 cols = row.find_all('td')
+#                 cols = [ele.text.strip() for ele in cols]
+#                 self.data.append(cols)
+
+#     def create_dataframe(self):
+#         columns = ['N°', 'Date', 'Artiste', 'Titre', 'Nombre de semaines n°1', 'Commentaires']
+#         data_musique = pd.DataFrame(self.data, columns=columns)
+#         data_musique = data_musique.dropna()
+#         data_musique = data_musique.reset_index(drop=True)
+#         return data_musique
+
+#     def save_to_csv(self, file_path, delimiter=';'):
+#         data_frame = self.create_dataframe()
+#         data_frame.to_csv(file_path, sep=delimiter)
+
+# # Scrapping 
+# url_de_la_page = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
+# target_date = "4/11/1984"
+# scraper = WikipediaScraper(url_de_la_page)
+# scraper.scrape_data(target_date)
+# scraper.save_to_csv("/home/ensai/Bureau/Conception de logiciel/projet/projet_conception_logiciel/Musique.csv")
