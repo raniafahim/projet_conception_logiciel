d0ca38cc60100376a44bd312702e17cbdc1c4e72
JP <yns@gmail.Com>
2024-03-14 17:12:30 +0100

    modif

--- carte_bis.py
+++ carte_bis.py
@@ -1,101 +1,77 @@
-
-
 import pandas as pd
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.lib.units import inch, mm
+from reportlab.lib import colors
 import qrcode
-#from api import data_musique
 from datetime import datetime
 
-from textwrap import wrap
-
-
 data = {
-    'Artiste': ['Artiste1', 'Artiste2', 'Artiste3','Peter et SloanePeter et SloanePeter et SloanePeter et SloanePeter et SloanePeter et Sloane ','Artiste4'],
-    'Titre': ['Titre1', 'Titre2', 'Titre3',"Besoin de rien, envie de toi",'Titre4',],
-    'Date': ["04/11/1984", "4/11/1984", "04/11/1984","04/11/1984","06/01/1985"],
-    'Audio': ['http://lien-audio-1', 'http://lien-audio-2', 'http://lien-au"Besoin de rien, envie de toi"dio-3', "https://p.scdn.co/mp3-preview/391b635047403beaec61ac63d502ae64ebd84f31?cid=4d3bffd173c84f21ab47981ba6cd15f8",'http://lien-audio-4']
+    'Artiste': ['Artiste1', 'Artiste2', 'Artiste3', 'Peter et Sloane', 'Artiste4'],
+    'Titre': ['Titre1', 'Titre2', 'Titre3', "Besoin de rien, envie de toi", 'Titre4'],
+    'Date': ["04/11/1984", "4/11/1984", "04/11/1984", "04/11/1984", "06/01/1985"],
+    'Audio': [
+        'http://lien-audio-1', 'http://lien-audio-2', 'http://lien-audio-3',
+        "https://p.scdn.co/mp3-preview/391b635047403beaec61ac63d502ae64ebd84f31?cid=4d3bffd173c84f21ab47981ba6cd15f8",
+        'http://lien-audio-4'
+    ]
 }
-
-
 ma_table_test = pd.DataFrame(data)
 
-
-
-def generer_carte(pdf, artiste, titre, date, audio_url):
-    # Convertir la date en objet datetime
+def generer_carte(pdf, artiste, titre, date, audio_url, verso=False):
     date_obj = datetime.strptime(date, "%d/%m/%Y")
     
-    # Définir les couleurs
-    couleur_bordure = (0.5, 0, 0.5)  # Violet
-    couleur_texte = (0, 0, 0)  # Noir
+    if verso:
+        # Paramètres du verso de la carte
+        pdf.setFillColorRGB(0.8, 0.5, 0.5)  # Couleur de fond claire pour le verso
+        pdf.rect(10, 10, 280, 180, fill=True, stroke=False)
+        
+        # Informations de l'artiste et titre
+        pdf.setFillColorRGB(0, 0, 0)  # Noir pour le texte
+        pdf.setFont("Helvetica-Bold", 24)
+        pdf.drawString(15, 150, date.split('/')[2])  # Année
+        
+        pdf.setFont("Helvetica", 18)
+        pdf.drawString(15, 120, titre)
+        
+        pdf.setFont("Helvetica", 12)
+        pdf.drawString(15, 90, artiste)
     
-    # Taille de la carte
-    largeur_carte = 300
-    hauteur_carte = 200
-    
-    # Dessiner la bordure de la carte
-    pdf.setStrokeColorRGB(*couleur_bordure)
-    pdf.rect(10, 10, largeur_carte, hauteur_carte)
-    
-    # Dessiner le recto de la carte avec le QR code
-    pdf.rect(15, 15, 270, 170)
-    
-    # Ajouter le QR code au recto de la carte
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=6,
-        border=1,
-    )
-    qr.add_data(audio_url)
-    qr.make(fit=True)
-    qr_img = qr.make_image(fill_color="black", back_color="white")
-    pdf.drawInlineImage(qr_img, 70, 20, width=150, height=150)
-    
-    # Dessiner le verso de la carte
-    pdf.showPage()
-    
-    # Dessiner la bordure du verso de la carte
-    pdf.setStrokeColorRGB(*couleur_bordure)
-    pdf.rect(10, 10, largeur_carte, hauteur_carte)
-    
-    # Ajouter le titre de l'artiste au verso
-    pdf.setFillColorRGB(*couleur_texte)
-    pdf.setFont("Helvetica-Bold", 18)
-    artiste_lines = wrap(artiste, width=30)  # Ajuster pour tenir dans la carte
-    artiste_start_y = 160
-    for line in artiste_lines:
-        pdf.drawCentredString(largeur_carte / 2, artiste_start_y, line)
-        artiste_start_y -= 15
-    
-    # Ajouter la date de la chanson au verso
-    pdf.setFont("Helvetica", 14)
-    pdf.drawCentredString(largeur_carte / 2, 150, date_obj.strftime("%d/%m/%Y"))
-    
-    # Ajouter le titre de la chanson au verso
-    pdf.setFont("Helvetica-Bold", 14)
-    titre_lines = wrap(titre, width=30)  # Ajuster pour tenir dans la carte
-    titre_start_y = 110
-    for line in titre_lines:
-        pdf.drawCentredString(largeur_carte / 2, titre_start_y, line)
-        titre_start_y -= 15
-
+    else:
+        # Paramètres du recto de la carte avec le QR code
+        pdf.setFillColorRGB(0.8, 0.1, 0.2)  # Blanc pour le fond du recto
+        pdf.rect(10, 10, 280, 180, fill=True, stroke=False)
+        
+        # QR Code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=1,
+        )
+        qr.add_data(audio_url)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        
+        # Position du QR code au centre
+        qr_size = 80 * mm
+        qr_x = (280 - qr_size) / 2
+        qr_y = (180 - qr_size) / 2 + 20
+        pdf.drawInlineImage(img, qr_x, qr_y, width=qr_size, height=qr_size)
 
 def generer_pdf(data_frame):
-    pdf_path = "playlist_test_bis.pdf"
-    pdf = canvas.Canvas(pdf_path, pagesize=letter)
+    pdf_path = "playlist_amelioree3.pdf"
+    pdf = canvas.Canvas(pdf_path, pagesize=landscape(letter))
 
     for index, row in data_frame.iterrows():
-        artiste = row['Artiste']
-        titre = row['Titre']
-        date = row['Date']
-        audio_url = row['Audio']
-
-        generer_carte(pdf, artiste, titre, date, audio_url)
+        generer_carte(pdf, row['Artiste'], row['Titre'], row['Date'], row['Audio'], verso=False)
+        pdf.showPage()  # Créer une nouvelle page pour le verso
+        generer_carte(pdf, row['Artiste'], row['Titre'], row['Date'], row['Audio'], verso=True)
+        pdf.showPage()  # Préparer la page suivante pour une nouvelle carte
 
     pdf.save()
 
-
 if __name__ == "__main__":
     generer_pdf(ma_table_test)
+
+
--- tests_unitaires.py
+++ tests_unitaires.py
@@ -1,51 +1,51 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from apy.py import SpotifyAPI  
+#from api.py import SpotifyAPI  
 
-class TestSpotifyAPI(unittest.TestCase):
-    def setUp(self):
-        self.spotify_api = SpotifyAPI()
+# class TestSpotifyAPI(unittest.TestCase):
+#     def setUp(self):
+#         self.spotify_api = SpotifyAPI()
 
-    @patch('spotipy.Spotify.search')
-    def test_recherche_piste(self, mock_search):
-        # Mock de la réponse de l'API Spotify
-        mock_search.return_value = {'tracks': {'items': [{'uri': 'mocked_uri'}]}}
+#     @patch('spotipy.Spotify.search')
+#     def test_recherche_piste(self, mock_search):
+#         # Mock de la réponse de l'API Spotify
+#         mock_search.return_value = {'tracks': {'items': [{'uri': 'mocked_uri'}]}}
 
-        # Test de la méthode recherche_piste
-        uri = self.spotify_api.recherche_piste('MockedTitle', 'MockedArtist')
-        self.assertEqual(uri, 'mocked_uri')
+#         # Test de la méthode recherche_piste
+#         uri = self.spotify_api.recherche_piste('MockedTitle', 'MockedArtist')
+#         self.assertEqual(uri, 'mocked_uri')
 
-    @patch('spotipy.Spotify.track')
-    def test_obtenir_audio(self, mock_track):
-        # Mock de la réponse de l'API Spotify
-        mock_track.return_value = {'preview_url': 'mocked_url'}
+#     @patch('spotipy.Spotify.track')
+#     def test_obtenir_audio(self, mock_track):
+#         # Mock de la réponse de l'API Spotify
+#         mock_track.return_value = {'preview_url': 'mocked_url'}
 
-        # Test de la méthode obtenir_audio
-        url = self.spotify_api.obtenir_audio('mocked_uri')
-        self.assertEqual(url, 'mocked_url')
+#         # Test de la méthode obtenir_audio
+#         url = self.spotify_api.obtenir_audio('mocked_uri')
+#         self.assertEqual(url, 'mocked_url')
 
-    @patch('spotify_api_module.SpotifyAPI.recherche_piste')
-    @patch('spotify_api_module.SpotifyAPI.obtenir_audio')
-    def test_renvoyer_piste_audio(self, mock_obtenir_audio, mock_recherche_piste):
-        # Mock des méthodes internes
-        mock_recherche_piste.return_value = 'mocked_uri'
-        mock_obtenir_audio.return_value = 'mocked_url'
+#     @patch('spotify_api_module.SpotifyAPI.recherche_piste')
+#     @patch('spotify_api_module.SpotifyAPI.obtenir_audio')
+#     def test_renvoyer_piste_audio(self, mock_obtenir_audio, mock_recherche_piste):
+#         # Mock des méthodes internes
+#         mock_recherche_piste.return_value = 'mocked_uri'
+#         mock_obtenir_audio.return_value = 'mocked_url'
 
-        # Test de la méthode renvoyer_piste_audio
-        url = self.spotify_api.renvoyer_piste_audio('MockedTitle', 'MockedArtist')
-        self.assertEqual(url, 'mocked_url')
+#         # Test de la méthode renvoyer_piste_audio
+#         url = self.spotify_api.renvoyer_piste_audio('MockedTitle', 'MockedArtist')
+#         self.assertEqual(url, 'mocked_url')
 
-    @patch('spotify_api_module.SpotifyAPI.recherche_piste')
-    def test_renvoyer_piste_audio_not_found(self, mock_recherche_piste):
-        # Mock de la méthode recherche_piste pour simuler un résultat non trouvé
-        mock_recherche_piste.return_value = None
+#     @patch('spotify_api_module.SpotifyAPI.recherche_piste')
+#     def test_renvoyer_piste_audio_not_found(self, mock_recherche_piste):
+#         # Mock de la méthode recherche_piste pour simuler un résultat non trouvé
+#         mock_recherche_piste.return_value = None
 
-        # Test de la méthode renvoyer_piste_audio pour le cas où la piste n'est pas trouvée
-        url = self.spotify_api.renvoyer_piste_audio('NonExistentTitle', 'NonExistentArtist')
-        self.assertIsNone(url)
+#         # Test de la méthode renvoyer_piste_audio pour le cas où la piste n'est pas trouvée
+#         url = self.spotify_api.renvoyer_piste_audio('NonExistentTitle', 'NonExistentArtist')
+#         self.assertIsNone(url)
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
 
 
 
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 
 import unittest
 from unittest.mock import MagicMock, patch
-from code_propre.py import WikipediaScraper  
+from scrapping import WikipediaScraper  
 
 class TestWikipediaScraper(unittest.TestCase):
     def setUp(self):
@@ -61,7 +61,7 @@ class TestWikipediaScraper(unittest.TestCase):
         self.target_date = "4/11/1984"
         self.scraper = WikipediaScraper(self.url)
 
-    @patch('wikipedia_scraper_module.WikipediaScraper.create_dataframe')
+    @patch('scrapping.WikipediaScraper.create_dataframe')
     def test_scrape_data(self, mock_create_dataframe):
         # Mock de la méthode create_dataframe
         mock_create_dataframe.return_value = MagicMock()
@@ -72,7 +72,7 @@ class TestWikipediaScraper(unittest.TestCase):
         mock_create_dataframe.assert_called_once()
 
     @patch('pandas.DataFrame.to_csv')
-    @patch('wikipedia_scraper_module.WikipediaScraper.create_dataframe')
+    @patch('scrapping.WikipediaScraper.create_dataframe')
     def test_save_to_csv(self, mock_create_dataframe, mock_to_csv):
         # Mock de la méthode create_dataframe et de la méthode to_csv
         mock_create_dataframe.return_value = MagicMock()
@@ -85,7 +85,7 @@ class TestWikipediaScraper(unittest.TestCase):
         mock_to_csv.assert_called_once_with(file_path, sep=';')
 
     @patch('requests.get')
-    @patch('wikipedia_scraper_module.WikipediaScraper.create_dataframe')
+    @patch('scrapping.WikipediaScraper.create_dataframe')
     def test_init(self, mock_create_dataframe, mock_requests_get):
         # Mock de la méthode create_dataframe et de la méthode requests.get
         mock_create_dataframe.return_value = MagicMock()
@@ -103,77 +103,5 @@ if __name__ == '__main__':
 
 
 
-
-# class SpotifyAPI:
-#     def __init__(self):
-#         self.sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-
-#     def recherche_piste(self, titre, artiste):
-#         query = f"{titre} {artiste}"
-#         results = self.sp.search(query, type='track', limit=1)
-#         if results['tracks']['items']:
-#             return results['tracks']['items'][0]['uri']
-#         else:
-#             return None
-
-#     def obtenir_audio(self, uri):
-#         track = self.sp.track(uri)
-#         return track['preview_url']
-
-#     def renvoyer_piste_audio(self, titre, artiste):
-#         uri = self.recherche_piste(titre, artiste)
-
-#         if uri:
-#             url = self.obtenir_audio(uri)
-#             print(url)
-#             return url
-#         else:
-#             print(f"Piste non trouvée pour {titre} de {artiste}")
-#             return None
-
-
-
-# # Exemple avec une recherche de piste
-# spotify_api = SpotifyAPI()
-# titre = "Shape of You"
-# artiste = "Ed Sheeran"
-# spotify_api.renvoyer_piste_audio(titre, artiste)
     
 
-
-# class WikipediaScraper:
-#     def __init__(self, url):
-#         self.url = url
-#         self.response = requests.get(self.url)
-#         self.html = self.response.content
-#         self.soup = BeautifulSoup(self.html, 'html.parser')
-#         self.table_rows = self.soup.find_all('tr')
-#         self.data = []
-#         self.scraping_started = False
-
-#     def scrape_data(self, target_date):
-#         for row in self.table_rows:
-#             if target_date in row.text:
-#                 self.scraping_started = True
-#             if self.scraping_started:
-#                 cols = row.find_all('td')
-#                 cols = [ele.text.strip() for ele in cols]
-#                 self.data.append(cols)
-
-#     def create_dataframe(self):
-#         columns = ['N°', 'Date', 'Artiste', 'Titre', 'Nombre de semaines n°1', 'Commentaires']
-#         data_musique = pd.DataFrame(self.data, columns=columns)
-#         data_musique = data_musique.dropna()
-#         data_musique = data_musique.reset_index(drop=True)
-#         return data_musique
-
-#     def save_to_csv(self, file_path, delimiter=';'):
-#         data_frame = self.create_dataframe()
-#         data_frame.to_csv(file_path, sep=delimiter)
-
-# # Scrapping 
-# url_de_la_page = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
-# target_date = "4/11/1984"
-# scraper = WikipediaScraper(url_de_la_page)
-# scraper.scrape_data(target_date)
-# scraper.save_to_csv("/home/ensai/Bureau/Conception de logiciel/projet/projet_conception_logiciel/Musique.csv")
