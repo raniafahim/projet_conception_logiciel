8c2c19a85f4b6f51a1aecdde00c51c318ae69f68
JP <yns@gmail.Com>
2024-03-15 16:29:56 +0100

    yo

--- tests_unitaires.py
+++ tests_unitaires.py
@@ -53,52 +53,36 @@ import unittest
 from unittest.mock import MagicMock, patch
 from scrapping import WikipediaScraper  
 
+
 class TestWikipediaScraper(unittest.TestCase):
+    @classmethod
     def setUp(self):
         self.url = "https://fr.wikipedia.org/wiki/Liste_des_singles_num%C3%A9ro_un_en_France"
         self.target_date = "4/11/1984"
+        self.file_path = "test_musique.csv"
         self.scraper = WikipediaScraper(self.url)
 
-    @patch('scrapping.WikipediaScraper.create_dataframe')
-    def test_scrape_data(self, mock_create_dataframe):
-        # Mock de la méthode create_dataframe
-        mock_create_dataframe.return_value = MagicMock()
-
-        # Test de la méthode scrape_data
+    def test_scrape_data(self):
         self.scraper.scrape_data(self.target_date)
-        self.assertTrue(self.scraper.scraping_started)
-        mock_create_dataframe.assert_called_once()
+        self.assertTrue(len(self.scraper.data) > 0, "Aucune donnée scrapée")
 
-    @patch('pandas.DataFrame.to_csv')
-    @patch('scrapping.WikipediaScraper.create_dataframe')
-    def test_save_to_csv(self, mock_create_dataframe, mock_to_csv):
-        # Mock de la méthode create_dataframe et de la méthode to_csv
-        mock_create_dataframe.return_value = MagicMock()
-        mock_to_csv.return_value = None
 
-        # Test de la méthode save_to_csv
-        file_path = "/home/ensai/Bureau/Conception de logiciel/projet/projet_conception_logiciel/Musique.csv"
-        self.scraper.save_to_csv(file_path)
-        mock_create_dataframe.assert_called_once()
-        mock_to_csv.assert_called_once_with(file_path, sep=';')
+    def test_save_to_csv(self):
+        self.scraper.save_to_csv(self.file_path)
+        self.assertTrue(os.path.exists(self.file_path), "Le fichier CSV n'a pas été créé")
 
-    @patch('requests.get')
-    @patch('scrapping.WikipediaScraper.create_dataframe')
-    def test_init(self, mock_create_dataframe, mock_requests_get):
-        # Mock de la méthode create_dataframe et de la méthode requests.get
-        mock_create_dataframe.return_value = MagicMock()
-        mock_requests_get.return_value = MagicMock()
+    def test_create_dataframe(self):
+        df = self.scraper.create_dataframe()
+        self.assertIsNotNone(df, "Le dataframe n'a pas été créé")
 
-        # Test de la méthode init
-        scraper = WikipediaScraper(self.url)
-        self.assertIsNotNone(scraper.response)
-        mock_create_dataframe.assert_called_once()
-        mock_requests_get.assert_called_once_with(self.url)
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(cls.file_path):
+            os.remove(cls.file_path)
 
 if __name__ == '__main__':
     unittest.main()
-
-
 
 
     
